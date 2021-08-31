1198d608a22c643960298544022bfececba4e2cb
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-31 19:07:28 +0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "The Wrath of GatsbyJS",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
+};
